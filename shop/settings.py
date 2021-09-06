8916da0f597ca3e6804f536ba08fe59fd2c1d343
G0udini <ruslan.shabunin2010@yandex.ru>
2021-09-06 22:07:43 +0300

    add cart object

--- shop/settings.py
+++ shop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main.apps.MainConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+# Default session cart
+
+CART_SESSION_ID = "cart"
