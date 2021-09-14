c9e5075d911c9a6e6614c558c6a215895e7ccaaa
G0udini <ruslan.shabunin2010@yandex.ru>
2021-09-14 23:19:30 +0300

    add payment app

---
+++ payment/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import payment_canceled, payment_done, payment_process
+
+app_label = "payment"
+
+urlpatterns = [
+    path("process/", payment_process, name="process"),
+    path("done/", payment_done, name="done"),
+    path("canceled/", payment_canceled, name="canceled"),
+]
--- shop/settings.py
+++ shop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "main.apps.MainConfig",
     "cart.apps.CartConfig",
     "orders.apps.OrdersConfig",
+    "payment.apps.PaymentConfig",
 ]
 
 MIDDLEWARE = [
@@ -140,3 +141,18 @@ CART_SESSION_ID = "cart"
 # Default smtp server
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Braintree settings
+
+BRAINTREE_MERCHANT_ID = os.getenv("BRAINTREE_MERCHANT_ID")
+BRAINTREE_PUBLIC_KEY = os.getenv("BRAINTREE_PUBLIC_KEY")
+BRAINTREE_PRIVATE_KEY = os.getenv("BRAINTREE_PRIVATE_KEY")
+
+from braintree import Configuration, Environment
+
+Configuration.configure(
+    Environment.Sandbox,
+    BRAINTREE_MERCHANT_ID,
+    BRAINTREE_PUBLIC_KEY,
+    BRAINTREE_PRIVATE_KEY,
+)
