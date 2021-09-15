982b1ba17f097dca06dd23e491c9b20345539149
G0udini <ruslan.shabunin2010@yandex.ru>
2021-09-15 18:27:23 +0300

    add csv order file gen

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,46 @@
+import csv
+import datetime
+
+from django.http import HttpResponse
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = f"attachment; filename={opts.verbose_name}.csv"
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = "Export to CSV"
+
+
+def order_detail(obj):
+    url = reverse("orders:admin_order_detail", args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 
 @admin.register(Order)
@@ -20,6 +56,8 @@ class OrderAdmin(admin.ModelAdmin):
         "paid",
         "created",
         "updated",
+        order_detail,
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import order_create
+from .views import order_create, admin_order_detail
 
 
 app_name = "orders"
 
 urlpatterns = [
+    path("admin/order/<int:order_id>/'", admin_order_detail, name="admin_order_detail"),
     path("create/", order_create, name="order_create"),
 ]
