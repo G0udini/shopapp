91756614be79879ca96fa25323078e8249dc6518
G0udini <ruslan.shabunin2010@yandex.ru>
2021-09-15 23:34:48 +0300

    add collectstatic and pdf gen

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import order_create, admin_order_detail
+from .views import admin_order_pdf, order_create, admin_order_detail
 
 
 app_name = "orders"
 
 urlpatterns = [
+    path("admin/order/<int:order_id>/pdf/", admin_order_pdf, name="admin_order_pdf"),
     path("admin/order/<int:order_id>/'", admin_order_detail, name="admin_order_detail"),
     path("create/", order_create, name="order_create"),
 ]
